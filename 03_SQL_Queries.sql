195e548c43402090e0552f7029a5786a1d6ca31f
Yesumani . Janga <yesumanijanga89@gmail.com>
2026-07-20 22:43:17 +0530

    Update 03_SQL_Queries.sql

--- 03_SQL_Queries.sql
+++ 03_SQL_Queries.sql
@@ -1,1 +1,23 @@
+SELECT * FROM Employee;
 
+SELECT * FROM Employee
+WHERE Salary > 40000;
+
+SELECT Emp_Name, Salary
+FROM Employee
+ORDER BY Salary DESC;
+
+SELECT Dept_ID, AVG(Salary)
+FROM Employee
+GROUP BY Dept_ID;
+
+SELECT MAX(Salary) FROM Employee;
+
+SELECT e.Emp_Name, d.Dept_Name
+FROM Employee e
+JOIN Department d
+ON e.Dept_ID = d.Dept_ID;
+
+CREATE VIEW Employee_Details AS
+SELECT Emp_Name, Salary
+FROM Employee;
