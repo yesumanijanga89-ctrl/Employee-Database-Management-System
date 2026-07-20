f68aeb2f4be233185916e1e619fe95a9e1120f41
Yesumani . Janga <yesumanijanga89@gmail.com>
2026-07-20 22:41:35 +0530

    Update 02_Insert_Data.sql

--- 02_Insert_Data.sql
+++ 02_Insert_Data.sql
@@ -1,1 +1,11 @@
+INSERT INTO Department VALUES
+(1,'HR'),
+(2,'IT'),
+(3,'Finance');
 
+INSERT INTO Employee VALUES
+(101,'Ravi',25,'Male',35000,2),
+(102,'Sita',28,'Female',42000,1),
+(103,'Kiran',30,'Male',50000,2),
+(104,'Anitha',27,'Female',45000,3),
+(105,'Rahul',32,'Male',60000,2);
